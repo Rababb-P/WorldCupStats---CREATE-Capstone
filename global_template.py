113b4619640f4f09cfaa81bf6b4ed19f19cef99f
Jeevan Sanchez <166963895+jeevan9s@users.noreply.github.com>
2024-08-13 13:51:01 -0400

    Add files via upload

---
+++ global_template.py
@@ -0,0 +1,65 @@
+# GLOBAL CHART TEMPLATE CODE 
+# REPLACE POSESSION STUFF IN DOUBLE QUOTATIONS WITH WHATEVER STAT YOU ARE WORKING WITH
+# CHANGE TITLES, LABELS ETC
+
+
+import matplotlib.pyplot as plt 
+import pandas as pd
+import os 
+
+os.makedirs("images", exist_ok=True)
+
+df = pd.read_csv(r"/Users/jeevansanchez/create2024/control/Fifa_world_cup_matches.csv")
+
+
+x = df.index
+countr_names = []
+statistic = {}
+
+
+# For percentages 
+df["possession team1"] = df["possession team1"].str.rstrip('%').astype('float')
+df["possession team2"] = df["possession team2"].str.rstrip('%').astype('float')
+
+for i in range(len(x)):
+  if df.loc[i, "team1"] not in countr_names:
+    countr_names.append(df.loc[i, "team1"])
+    statistic[df.loc[i, "team1"]] = 0
+for i in range(len(x)):
+  if df.loc[i, "team2"] not in countr_names:
+    countr_names.append(df.loc[i, "team2"])
+    statistic[df.loc[i, "team2"]] = 0
+
+for i in range(len(x)):
+   statistic[df.loc[i, "team1"]]+=df.loc[i, "possession team1"]
+   statistic[df.loc[i, "team2"]]+=df.loc[i, "possession team2"]
+
+   
+  
+cols = ['statName']
+
+
+statistic = pd.DataFrame.from_dict(statistic, orient='index', columns=cols)
+y_statistic = statistic["statName"]
+statistic.reset_index(inplace=True)
+statistic.rename(columns={"index": "Country"}, inplace=True)
+x_statistic = []
+
+
+for i in range(len(y_statistic)):
+  x_statistic.append(i)
+print(y_statistic)
+print(len(y_statistic))
+
+print(len(x_statistic))
+
+plt.figure(figsize=(16,14))
+plt.scatter(statistic["Country"],y_statistic)
+plt.xticks(rotation=45, ha='right')
+
+plt.title("Ball Possession vs Defensive Pressure")
+
+
+plot_path = os.path.join("images", "my_plot.png")
+plt.savefig(plot_path)
+plt.show()
