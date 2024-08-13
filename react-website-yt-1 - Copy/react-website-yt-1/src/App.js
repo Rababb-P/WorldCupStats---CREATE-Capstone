c3c6631ca04b5bbbbb2320d39bbed1b222ce3556
Yahya-Abbs <yahyabbas0@gmail.com>
2024-08-13 14:09:04 -0400

    w

---
+++ react-website-yt-1 - Copy/react-website-yt-1/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import logo from './logo.svg';
+import './App.css';
+import { Header } from './Elements/Header';
+import { Hero } from './Elements/Hero';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Hero />
+    </div>
+  );
+}
+
+export default App;
